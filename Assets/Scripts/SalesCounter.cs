61c37e5236f5997e7a92582b684148cbaf7147c3
paulsmithkc <paulsmithkc@gmail.com>
2017-05-27 13:20:47 -0500

    Hooking up sales counter script

---
+++ Assets/Scripts/SalesCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalesCounter : MonoBehaviour
+{
+    public List<Item> items;
+
+	// Use this for initialization
+	void Start ()
+    {
+        items = new List<Item>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        var item = other.gameObject.GetComponent<Item>();
+        if (item != null)
+        {
+            items.Add(item);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        var item = other.gameObject.GetComponent<Item>();
+        if (item != null)
+        {
+            items.Remove(item);
+        }
+    }
+}
