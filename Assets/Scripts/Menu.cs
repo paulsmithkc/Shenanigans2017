d8265b127789d3f0799ca6400ba117b7062abc2e
Eric Demello <egdemello@gmail.com>
2017-05-28 16:26:53 -0500

    First pass at splash screen

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+	void Start () {
+	}
+
+	public void DisplayMain () {
+		Application.LoadLevel ("Test");
+	}
+}
